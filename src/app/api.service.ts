45167e04f4253f4c7268de7265bbc25337e7f596
Andrew Giovengo <Andrew.Giovengo@cellularsales.com>
2023-03-30 13:18:51 -0400

    Add sample connection to API for get()

---
+++ src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiBaseUrl = 'https://localhost:7258';
+
+  constructor(private http: HttpClient) { }
+
+  public getExample(): Observable<any> {
+    return this.http.get<any>(`${this.apiBaseUrl}/SkateLog`);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkatingLogWeb';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getExample().subscribe(
+      (data) => {
+        console.log('API response:', data);
+      },
+      (error) => {
+        console.error('API error:', error);
+      }
+    );
+  }
 }
